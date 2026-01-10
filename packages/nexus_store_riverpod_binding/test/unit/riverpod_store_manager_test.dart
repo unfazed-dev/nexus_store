cb84329acd9a79f1218103410234376d7bcad35e
Evan F Pierre Louis <evan.pierrelouis@pm.me>
2026-01-10 21:04:25 +1100

    style(riverpod): remove unused imports in test files

--- packages/nexus_store_riverpod_binding/test/unit/riverpod_store_manager_test.dart
+++ packages/nexus_store_riverpod_binding/test/unit/riverpod_store_manager_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:nexus_store/nexus_store.dart';
 import 'package:nexus_store_riverpod_binding/nexus_store_riverpod_binding.dart';
 
 import '../helpers/mocks.dart';
